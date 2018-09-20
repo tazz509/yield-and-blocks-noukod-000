94649dcce2712bb020134a818c6a6186b06e6a05
carly <carly@noukod.com>
2018-09-20 18:38:59 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,17 @@
-def hello_t
- array.each do |name|
-   puts "#{name}"
-end
+def hello_t arr
+  if block_given?
+    i = 0
+
+    while i < arr.length
+        yield arr[i]
+        i = i + 1
+    end
+
+    arr
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
+hello_t(["Tim", "Tom", "Jim"])
